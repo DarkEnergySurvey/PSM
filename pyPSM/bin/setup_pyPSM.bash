000bbf6a200d4509aeda49c7abada25cc7ed8250
Douglas Tucker <dtucker@fnal.gov>
2013-05-17 23:12:43 +0000

    Added setup script for pyPSM

---
+++ pyPSM/bin/setup_pyPSM.bash
@@ -0,0 +1,5 @@
+# Be sure to set DESPHOTOSTDSMOD_HOME before sourcing this file.
+
+export PYTHONPATH=${DESPHOTOSTDSMOD_HOME}/pyPSM/python:${PYTHONPATH}
+export PATH=${DESPHOTOSTDSMOD_HOME}/pyPSM/python:${PATH}
+
