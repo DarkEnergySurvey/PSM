73117955b334fe4891309837ff5f1f2044ce9d6a
Douglas Tucker <dtucker@fnal.gov>
2013-05-24 00:01:00 +0000

    Finished fleshing out FITS output table to match schema of PSMFIT table in the NCSA DES db; changed bsolve, ksolve from strings to ints; added more statistical descriptions of fit (e.g., chi2, dof)

--- pyPSM/python/psm.py
+++ pyPSM/python/psm.py
@@ -2,7 +2,7 @@
 
 # Authors:   Brian Yanny and Douglas Tucker
 # Date:      17 May 2013
-# Updated:   18 May 2013
+# Updated:   23 May 2013
 
 # Description:
 #
@@ -34,6 +34,7 @@ import math
 import os
 import getopt
 import pyfits
+import datetime
 
 #---------------------------------------------------------------------------
 # Client usage.
@@ -64,6 +65,24 @@ def usage():
 
 def psm(inmatches,outak,bandid,niter,thresholdit,ksolve,bsolve):
 
+   # Some values which need to be passed either in the psm argument list
+   # or derived from info from the inmatches file...
+   # Note that mjdlo and mjdhi are only used for QA plots...
+   nite = '20130221'               # nite of observation
+   run = '20130523114356_20130221' # processing run name
+   project = 'OPS'                 # project name
+   mjdlo = -1.                     # time and date of earliest observation in fit (expressed as an MJD) 
+   mjdhi = -1.                     # time and date of latest observation in fit (expressed as an MJD) 
+   psmfit_id_last = 93234          # current largest psmfit_id in the PSMFIT table
+   psmversion = 'pyPSM_v0.1'       # psm version id
+   mag_type = 'mag_psf'            # type of magnitude used in fit (e.g., mag_psf, mag_aper_8, ...)
+   
+   # Currently, pyPSM always solves for the a coefficients.
+   # If, in the future, the option is added to choose whether
+   # or not the a coefficients will be solved for, an "--asolve"
+   # toggle should be added to Main method...
+   asolve = 1
+
    # Extract info from the list of arguments passed to psm.py...
    # First, convert certain arguments from strings to numbers...
    bandid = int(bandid)
@@ -84,54 +103,54 @@ def psm(inmatches,outak,bandid,niter,thresholdit,ksolve,bsolve):
    if fitband == 'u':
       cfitband = 'g'
       stdColorName = 'u-g'
-      bdefault = 0.0
+      bdefault = 0.06
       color0 = 1.39
       kdefault = 0.489
+      # The u-band kgoodLo and kgoodHi need verification...
+      kgoodLo = 0.2
+      kgoodHi = 0.8
    elif fitband == 'g':
       cfitband = 'r'
       stdColorName = 'g-r'
       bdefault = -0.1
       color0 = 0.53
       kdefault = 0.181
+      kgoodLo = 0.15
+      kgoodHi = 0.30
    elif fitband == 'r':
       cfitband = 'g'
       stdColorName = 'g-r'
       bdefault = -0.08
       color0 = 0.53
       kdefault = 0.095
+      kgoodLo = 0.06
+      kgoodHi = 0.20
    elif fitband == 'i':
       cfitband = 'z'
       stdColorName = 'i-z'
       bdefault = -0.3
       color0 = 0.09
       kdefault = 0.089
+      kgoodLo = 0.03
+      kgoodHi = 0.16
    elif fitband == 'z':
       cfitband = 'i'
       stdColorName = 'i-z'
       bdefault = -0.09
       color0 = 0.053
       kdefault = 0.089
+      kgoodLo = 0.03
+      kgoodHi = 0.15
    elif fitband == 'Y':
       cfitband = 'z'
       stdColorName = 'z-Y'
       bdefault = 0.26
       color0 = 0.05
       kdefault = 0.050
+      kgoodLo = 0.01
+      kgoodHi = 0.17
    #endif
 
-   # Solve for k coefficient?  Default is no.
-   if ksolve in ['True', 'true', 'T', 't', '1', 'yes', 'y', '1']:
-      ksolve = 'True'
-   else:
-      ksolve = 'False'
-   #endif
-
-   # Solve for b coefficients?  Default is no.
-   if bsolve in ['True', 'true', 'T', 't', '1', 'yes', 'y', '1']:
-      bsolve = 'True'
-   else:
-      bsolve = 'False'
-   #endif
 
    # The input matched file is inmatches; 
    # the output matched file (used for iterative clipping) is inmatches.tmp; 
@@ -154,12 +173,12 @@ def psm(inmatches,outak,bandid,niter,thresholdit,ksolve,bsolve):
    nccd = 62
    nparam = 2*nccd + 1
    nFreeParam = nccd
-   if ksolve=='True':
+   if ksolve==1:
       nFreeParam = nFreeParam + 1
-   if bsolve=='True':
+   if bsolve==1:
       nFreeParam = nFreeParam + nccd
-   
-   print ksolve, bsolve, nparam, nFreeParam
+   if verbose > 1:
+      print ksolve, bsolve, nparam, nFreeParam
 
    # Iteration loop...
    for iiter in range(0,niter):
@@ -168,6 +187,8 @@ def psm(inmatches,outak,bandid,niter,thresholdit,ksolve,bsolve):
       # matrix equation AA.XX=BB)...
       sum = 0.0
       sumsq = 0.0
+      sumchi = 0.0
+      sumchisq = 0.0
       ninfit = 0.0
       zv = numpy.zeros(nparam)
       II = numpy.array(numpy.identity(nparam))
@@ -200,10 +221,9 @@ def psm(inmatches,outak,bandid,niter,thresholdit,ksolve,bsolve):
          magstd = float(lsp[3])
          mag = float(lsp[14])
          magerr = float(lsp[15])
-         #weight = 1.0/(magerr+0.00001)/(magerr+0.00001)
-         #weight = 1.0
+         #totMagErr = math.sqrt(baseMagErr*baseMagErr + magerr*mag)
+         totMagErr = baseMagErr
          weight = 1.0/(baseMagErr*baseMagErr)
-         #print mag,magerr,weight
          exptime = float(lsp[11])
          airmass = float(lsp[12])
          dm  = (mag - defaultzp + 2.5*math.log(exptime,10)) - magstd
@@ -240,7 +260,7 @@ def psm(inmatches,outak,bandid,niter,thresholdit,ksolve,bsolve):
      
             dc = colorstd-color0
 
-            if bsolve=='True':
+            if bsolve==1:
                AA[iparam_b][iparam_b] += dc * dc * weight
                AA[0][iparam_b] += dc * airmass * weight
                AA[iparam_b][0] += dc * airmass * weight
@@ -264,7 +284,8 @@ def psm(inmatches,outak,bandid,niter,thresholdit,ksolve,bsolve):
       # Find any CCDs for which nstar==0
       badinds = numpy.hstack(numpy.nonzero(nstar==0))
 
-      # Set the a's, b's of any CCDs for which nstar=0 to signal values
+      # Set the a's, b's of any CCDs for which nstar=0 to signal values,
+      # reducing the number of free parameters as appropriate
       for iccd in range(0,len(badinds)):
 
          ccd = badinds[iccd] + 1
@@ -275,14 +296,23 @@ def psm(inmatches,outak,bandid,niter,thresholdit,ksolve,bsolve):
          AA[iparam_a,:] = 0.0
          AA[iparam_a][iparam_a] = 1.0
          BB[iparam_a] = -9999.00+25.00
+         # Since we always solve for a, any CCD with nstar=0
+         # reduces the number of free parameters by 1...
+         nFreeParam += -1
 
          # Fix b's...
          AA[iparam_b,:] = 0.0
          AA[iparam_b][iparam_b] = 1.0
-         BB[iparam_b] = -9999.00         
+         BB[iparam_b] = -9999.00
+         # Only reduce the number of free parameters if we are
+         # solving for the b term coefficients...
+         if bsolve == 1:
+            nFreeParam += -1
+         #endif
+            
       #endfor
 
-      if (ksolve=='False'):
+      if (ksolve==0):
          AA[0][0] = 1.0
          BB[0] = kdefault
          for iccd in range(0,int(nccd)):
@@ -322,11 +352,15 @@ def psm(inmatches,outak,bandid,niter,thresholdit,ksolve,bsolve):
          exptime=float(lsp[11])
          airmass=float(lsp[12])
          expnum=int(lsp[9])
-         # Note:  change befault to XX[nccd+ccd]
+         # Note:  should change befault to XX[nccd+ccd]
          dm = (mag + 2.5*math.log(exptime,10) - XX[ccd] - XX[0]*airmass - bdefault*(colorstd-color0)) - magstd
+         #dm = (mag + 2.5*math.log(exptime,10) - XX[ccd] - XX[0]*airmass - XX[nccd+ccd]*(colorstd-color0)) - magstd
          sum += dm
          sumsq += dm*dm
+         sumchi += dm/totMagErr
+         sumchisq += (dm/totMagErr)*(dm/totMagErr)
          ninfit += 1
+         # Should change to a sigma-clip...
          if abs(dm) < thresholdit:
             ofd2.write(l)
             resOutputLine = '%.4f,%.3f,%.4f,%.4f,%d,%d\n' % (dm,airmass,magstd,colorstd,ccd,expnum)
@@ -339,10 +373,24 @@ def psm(inmatches,outak,bandid,niter,thresholdit,ksolve,bsolve):
       ofd2.close()
       infile = outfile
       outfile = infile+'.tmp'
-      avg = float(sum/ninfit)
-      sigma = math.sqrt(sumsq/ninfit-avg*avg)
-      print 'k:'+str(XX[0])+' rms:'+str(sigma)+' n:'+str(ninfit)
-      print 'kerr:'+str(math.sqrt(AAinv[0][0]))
+      dof = ninfit - nFreeParam
+      photometricFlag = -1
+      if dof > 0:
+         avg = float(sum/ninfit)
+         sigma = math.sqrt(float(ninfit)/float(dof))*math.sqrt(sumsq/ninfit-avg*avg)
+         avgchi = float(sumchi/ninfit)
+         chisq = math.sqrt(float(ninfit)/float(dof))*math.sqrt(sumchisq/ninfit-avgchi*avgchi)
+         if (sigma < 0.025) and (XX[0] >= kgoodLo and XX[0] <= kgoodHi):  
+            photometricFlag = 1
+         else:
+            photometricFlag = 0
+         #endif
+         if verbose > 1:
+            print 'avg:'+str(avg)+'  avgchi:'+str(avgchi)+'  chisq:'+str(chisq)
+            print 'k:'+str(XX[0])+' rms:'+str(sigma)+' n:'+str(ninfit)
+            print 'kerr:'+str(math.sqrt(AAinv[0][0]))
+         #endif
+      #endif
 
    #endfor (iiter)
 
@@ -363,38 +411,83 @@ def psm(inmatches,outak,bandid,niter,thresholdit,ksolve,bsolve):
       print outputLine,
       ofd.write('b_'+band+' '+str(i)+' '+str('%.3f'%float(XX[nccd+i]))+'\n')
    ofd.close()
-   print XX[1:nccd+1]-25.
-   print XX[nccd+1:]
 
    # Output the results of fit into a FITS table file...
    os.system("/bin/rm -f "+outfitsfile)
-   ccds = numpy.arange(1,63)
-   print ccds
-   # Note:  verify errors are what we expect:
+   ccdidArray = numpy.arange(1,63)
+   psmfit_idArray = ccdidArray+psmfit_id_last
+   niteFormat = 'A%d' % len(nite)
+   niteArray = numpy.array([nite]*62)
+   mjdloArray = mjdlo*numpy.ones(nccd,dtype=numpy.int)
+   mjdhiArray = mjdhi*numpy.ones(nccd,dtype=numpy.int)
+   ccdidArray = numpy.arange(1,63)
+   filterFormat = 'A%d' % len(band)
+   filterArray = numpy.array([band]*62)
+   # Note:  verify that these errors are what we expect:
    errors = numpy.sqrt(numpy.diagonal(AAinv))
-   k    = XX[0]*numpy.ones(nccd,dtype=numpy.int)
-   kerr = errors[0]*numpy.ones(nccd,dtype=numpy.int)
-   print k
-   print kerr
-   a    = XX[1:nccd+1]-25.
-   aerr = errors[1:nccd+1]
-   b = XX[nccd+1:]
-   berr = errors[nccd+1:]
-   col1=pyfits.Column(name='ccdid', format='I', array=ccds)
-   col2=pyfits.Column(name='a', format='E', array=a)
-   col3=pyfits.Column(name='aerr', format='E', array=aerr)
-   col4=pyfits.Column(name='b', format='E', array=b)
-   col5=pyfits.Column(name='berr', format='E', array=berr)
-   col6=pyfits.Column(name='k', format='E', array=k)
-   col7=pyfits.Column(name='kerr', format='E', array=kerr)
-   cols=pyfits.ColDefs([col1, col2, col3, col4, col5, col6, col7])
+   kArray = XX[0]*numpy.ones(nccd,dtype=numpy.int)
+   kerrArray = errors[0]*numpy.ones(nccd,dtype=numpy.int)
+   aArray = XX[1:nccd+1]-25.
+   aerrArray = errors[1:nccd+1]
+   bArray = XX[nccd+1:]
+   berrArray = errors[nccd+1:]
+   rmsArray = sigma*numpy.ones(nccd,dtype=numpy.int)
+   chisqArray = chisq*numpy.ones(nccd,dtype=numpy.int)
+   dofArray = dof*numpy.ones(nccd,dtype=numpy.int)
+   photometricFlagArray = photometricFlag*numpy.ones(nccd,dtype=numpy.int)
+   psmversionFormat = 'A%d' % len(psmversion)
+   psmversionArray = numpy.array([psmversion]*62)
+   fit_timestamp = str(datetime.datetime.utcnow())
+   fit_timestampFormat = 'A%d' % len(fit_timestamp)
+   fit_timestampArray = numpy.array([fit_timestamp]*62)
+   cfilterFormat = 'A%d' % len(cfitband)
+   cfilterArray = numpy.array([cfitband]*62)
+   stdcolor0Array = color0*numpy.ones(nccd,dtype=numpy.int)
+   asolveArray = asolve*numpy.ones(nccd,dtype=numpy.int)
+   bsolveArray = bsolve*numpy.ones(nccd,dtype=numpy.int)
+   ksolveArray = ksolve*numpy.ones(nccd,dtype=numpy.int)
+   runFormat = 'A%d' % len(run)
+   runArray = numpy.array([run]*62)
+   projectFormat = 'A%d' % len(project)
+   projectArray = numpy.array([project]*62)
+   mag_typeFormat = 'A%d' % len(mag_type)
+   mag_typeArray = numpy.array([mag_type]*62)
+   
+   col1  = pyfits.Column(name='psmfit_id',      format='J', array=psmfit_idArray)
+   col2  = pyfits.Column(name='nite',           format=niteFormat, array=niteArray)
+   col3  = pyfits.Column(name='mjdlo',          format='E', array=mjdloArray)
+   col4  = pyfits.Column(name='mjdhi',          format='E', array=mjdhiArray)
+   col5  = pyfits.Column(name='ccdid',          format='I', array=ccdidArray)
+   col6  = pyfits.Column(name='filter',         format=filterFormat, array=filterArray)
+   col7  = pyfits.Column(name='a',              format='E', array=aArray)
+   col8  = pyfits.Column(name='aerr',           format='E', array=aerrArray)
+   col9  = pyfits.Column(name='b',              format='E', array=bArray)
+   col10 = pyfits.Column(name='berr',           format='E', array=berrArray)
+   col11 = pyfits.Column(name='k',              format='E', array=kArray)
+   col12 = pyfits.Column(name='kerr',           format='E', array=kerrArray)
+   col13 = pyfits.Column(name='rms',            format='E', array=rmsArray)
+   col14 = pyfits.Column(name='chi2',           format='E', array=chisqArray)
+   col15 = pyfits.Column(name='dof',            format='I', array=dofArray)
+   col16 = pyfits.Column(name='photomtricflag', format='I', array=photometricFlagArray)
+   col17 = pyfits.Column(name='psmversion',     format=psmversionFormat, array=psmversionArray)
+   col18 = pyfits.Column(name='fit_timestamp',  format=fit_timestampFormat, array=fit_timestampArray)
+   col19 = pyfits.Column(name='cfilter',        format=cfilterFormat, array=cfilterArray)
+   col20 = pyfits.Column(name='stdcolor0',      format='E', array=stdcolor0Array)
+   col21 = pyfits.Column(name='asolve',         format='I', array=asolveArray)
+   col22 = pyfits.Column(name='bsolve',         format='I', array=bsolveArray)
+   col23 = pyfits.Column(name='ksolve',         format='I', array=ksolveArray)
+   col24 = pyfits.Column(name='run',            format=runFormat, array=runArray)
+   col25 = pyfits.Column(name='project',        format=projectFormat, array=projectArray)
+   col26 = pyfits.Column(name='mag_type',       format=mag_typeFormat, array=mag_typeArray)
+
+   cols=pyfits.ColDefs([col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15, col16, col17, col18, col19, col20, col21, col22, col23, col24, col25, col26])
    tbhdu=pyfits.new_table(cols)
    tbhdu.writeto(outfitsfile)
 
 
    print "That's all, folks!"
  
-   sys.exit
+   sys.exit(0)
  
 #---------------------------------------------------------------------------
 # Main method:
@@ -422,15 +515,15 @@ if __name__ == "__main__":
       sys.exit(1)
    #end try
 
-   verbose = 0        # Default value for verbosity
-   bsolve  = 'False'  # Default value for bsolve
-   ksolve  = 'False'  # Default value for ksolve
+   verbose = 0     # Default value for verbosity
+   bsolve  = 0     # Default value for bsolve (0=no, 1=yes)
+   ksolve  = 0     # Default value for ksolve (0=no, 1=yes)
 
    for o, a in opts:
       if o == '--bsolve':
-         bsolve = 'True'
+         bsolve = 1
       elif o == '--ksolve':
-         ksolve = 'True'
+         ksolve = 1
       elif o in ('-h', '--help'):
          usage()
          sys.exit(0)
