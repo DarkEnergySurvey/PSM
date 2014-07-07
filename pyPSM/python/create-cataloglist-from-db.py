e46b12dd4c13ef2899fe3b7f13902abfa656e348
Douglas Tucker <dtucker@fnal.gov>
2014-07-07 19:50:50 +0000

    Updated argument list and typed arguments reqnum, attempt, and verbose as integers

--- pyPSM/python/create-cataloglist-from-db.py
+++ pyPSM/python/create-cataloglist-from-db.py
@@ -11,7 +11,7 @@
     
     create-cataloglist-from-db.py --help
     
-    create-cataloglist-from-db.py -s db-destest --nite 20140202 --band g --provenanceFile psmcats-20140202-g-r47p01.list --verbose 1
+    create-cataloglist-from-db.py -s db-destest --nite 20131002 --reqnum 3 --attempt 1 --band g --outputFile psmcats-20131002-g-r03p01.list --verbose 1
     
     """
 
@@ -29,10 +29,12 @@ def main():
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('--section','-s',default='db-destest',
                         help='section in the .desservices file w/ DB connection information')
-    parser.add_argument('--nite', help='nite to be queried', default=20140901)
+    parser.add_argument('--nite', help='nite to be queried', default=20131002)
+    parser.add_argument('--reqnum', help='request number to be queried', default=3, type=int)
+    parser.add_argument('--attempt', help='processing attempt to be queried (***CURRENTLY NOT USED***)', default=1, type=int)
     parser.add_argument('--band', help='band to be queried', default='g', choices=['u','g','r','i','z','Y'])
-    parser.add_argument('--provenanceFile', help='ASCII list file containing list of unique names of all catalog files returned by query', default='psmcats.list')
-    parser.add_argument('--verbose', help='verbosity level of output to screen (0,1,2,...)', default=0)
+    parser.add_argument('--outputFile', help='ASCII list file containing list of unique names of all catalog files (plus exposure and image-based info) returned by query', default='psmcats.list')
+    parser.add_argument('--verbose', help='verbosity level of output to screen (0,1,2,...)', default=0, type=int)
                         
     args = parser.parse_args()
                         
@@ -64,14 +66,14 @@ def callcreatecataloglistfromdb(args):
                i.saturate as image_sat_level, c.filetype, i.crpix1, i.crpix2, i.naxis2, i.naxis2,
                rasicam.gskyphot, rasicam.gskyvar, rasicam.lskyphot, rasicam.lskyvar, rasicam.source
         FROM EXPOSURE e, IMAGE i, WDF, CATALOG c, gruendl.rasicam_decam@desoper rasicam,
-             (select distinct filename from se_object where nite=%s and reqnum = 64 and band='%s') s
+             (select distinct filename from se_object where nite=%s and reqnum=%d and band='%s') s
         WHERE e.expnum=i.expnum AND
               i.filename=wdf.parent_name AND
               wdf.child_name=c.filename AND
               c.filename=s.filename AND
               e.filename=rasicam.exposurename AND
               c.filetype='cat_finalcut'
-        ORDER BY s.filename""" % (args.nite,args.band)
+        ORDER BY s.filename""" % (args.nite,args.reqnum,args.band)
     
         #AND
         #   (UPPER(rasicam.source)='HEADER') AND (UPPER(rasicam.gskyphot)='T')
@@ -86,8 +88,8 @@ def callcreatecataloglistfromdb(args):
     
     if args.verbose > 0: print "query took", time.time()-t0, "seconds"
     
-    print "Outputting query results to file", args.provenanceFile
-    with open(args.provenanceFile,'w') as csvFile:
+    print "Outputting query results to file", args.outputFile
+    with open(args.outputFile,'w') as csvFile:
         writer = csv.writer(csvFile,delimiter=',',  quotechar='|',
                             lineterminator='\n', quoting=csv.QUOTE_MINIMAL)
         #First output header...
